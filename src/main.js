d4dfafd0326b9cf4c6a1c318c40fa770a2c8dc2b
unknown <542831555@qq.com>
2021-04-12 09:14:37 +0800

    初始化目录结构

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
-Vue.config.productionTip = false
-
+import '@/styles/index.less'
+// 引入全局样式文件
+Vue.config.productionTip = false // 禁止生产环境提示
 new Vue({
   router,
   render: h => h(App)
